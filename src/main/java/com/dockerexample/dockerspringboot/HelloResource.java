5940d8ef08e16bc4ad879473b0f51544aaea93cb
Neelesh Varma Dhanalakota <neeleshvarmadhanalakota@Neeleshs-MacBook-Pro.local>
2018-06-06 19:12:25 -0500

    initial commit

---
+++ src/main/java/com/dockerexample/dockerspringboot/HelloResource.java
@@ -0,0 +1,18 @@
+package com.dockerexample.dockerspringboot;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+	
+	@GetMapping
+	public String hello()
+	{
+		return "Hello eGiants";
+	}
+
+}
